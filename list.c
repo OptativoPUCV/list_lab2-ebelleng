5bc42f7cbd35ab8dda959373ef026f2c6dcd9e71
Replit user <replituser@example.com>
2021-03-26 00:36:41 +0000

    new try

--- list.c
+++ list.c
@@ -54,14 +54,11 @@ void * lastList(List * list) {
 }
 
 void * prevList(List * list) {
-  if( list->current ){
+  if( list->current != NULL && list->current->prev != NULL){
     list->current = list->current->prev;
-    return list->current;
+    return (void*)list->current->data;
   }
-  else{
-    return NULL;
-  }
-  
+  return NULL;  
 }
 
 void pushFront(List * list, const void * data) {
